cd8566eb724113c7ed91e77fcea8d51cfd278fa6
samuele-galli <samuele.aggeggio@gmail.com>
2026-06-16 18:20:19 +0200

    do some Tree planting with tescase

--- Divide_And_Conquer/Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.cpp
+++ Divide_And_Conquer/Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.cpp
@@ -30,10 +30,9 @@ using namespace std;
 #include <iostream>
 #include <vector>
 #include <optional>
+#include <unordered_map>
 
 /*
-TODO Implementa Print
-TODO Implementa Testcase
 TODO Risolvi problema
 */
 // Definition for a binary tree node.
@@ -47,35 +46,44 @@ struct TreeNode
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
+/*
+post-order(left - right - root)
+in-order(left - root - right)
+*/
+
 class Solution
 {
+private:
+    TreeNode *treefing(const vector<int> &postorder,
+                       int start, int end,
+                       size_t &selector, unordered_map<int, size_t> &aux)
+    {
+        if (start > end)
+            return (nullptr);
+
+        TreeNode *head;
+        int pivot = static_cast<int>(aux[postorder[selector]]);
+
+        head = new TreeNode(postorder[selector]);
+        selector--;
+        head->right = treefing(postorder, pivot + 1, end, selector, aux);
+        head->left = treefing(postorder, start, pivot - 1, selector, aux);
+        return (head);
+    }
+
 public:
     TreeNode *buildTree(vector<int> &inorder, vector<int> &postorder)
     {
+        unordered_map<int, size_t> aux;
+        size_t selector;
+
+        selector = postorder.size() - 1;
+        for (size_t i = 0; i < inorder.size(); i++)
+            aux[inorder[i]] = i;
+        TreeNode *head = treefing(postorder, 0, postorder.size() - 1, selector, aux);
+        return (head);
     }
 };
-
-void create_Tree(TreeNode *&head, vector<optional<int>> &Tree, size_t &i)
-{
-    if (i == Tree.size())
-        return;
-
-    TreeNode *Node;
-
-    if (Tree[i] != nullopt)
-        Node = new TreeNode(Tree[i].value());
-    else
-    {
-        head = nullptr;
-        i++;
-        return;
-    }
-
-    head = Node;
-    i++;
-    create_Tree(head->left, Tree, i);
-    create_Tree(head->right, Tree, i);
-}
 
 void Delete_Tree(TreeNode *&head)
 {
@@ -88,22 +96,65 @@ void Delete_Tree(TreeNode *&head)
     head = nullptr;
 }
 
-int main()
+void printing(TreeNode *&temp, string &result)
+{
+    if (!temp)
+    {
+        result += "NULL, ";
+        return;
+    }
+    result += to_string(temp->val) + ", ";
+    printing(temp->left, result);
+    printing(temp->right, result);
+}
+
+string print_tree(TreeNode *&head, bool print)
+{
+    TreeNode *temp = head;
+    string result = "{";
+    printing(temp, result);
+    if (!result.empty())
+    {
+        result.pop_back();
+        result.pop_back();
+    }
+    result += "}\n";
+    if (print == false)
+    {
+        cout << "L'albero e composto da " << result << endl;
+    }
+    return (result);
+}
+
+void Testcase(const string &Tree,
+              vector<int> &inorder, vector<int> &postorder)
 {
     Solution s;
-    TreeNode *head = nullptr;
-    TreeNode *new_head = nullptr;
+    TreeNode *new_head;
+
+    new_head = s.buildTree(inorder, postorder);
+    cout << "excepted = " << Tree;
+    if (Tree == print_tree(new_head, true))
+        cout << "head e new_head = \"UGUALI\"\n";
+    else
+    {
+        cout << "head e new_head = \"DIVERSI\"\n";
+        cout << "new_head =  " << print_tree(new_head, true);
+    }
+    Delete_Tree(new_head);
+    cout << "-------------------------------------------\n";
+}
+
+int main()
+{
     vector<optional<int>> Tree;
     vector<int> inorder;
     vector<int> postorder;
-    size_t i = 0;
 
-    //Tescase(vettore albero,head,vettore inorder, vettore postorder);
-    Tree = {3, 9, nullopt, nullopt, 20, 15, 7, nullopt, nullopt};
-    create_Tree(head, Tree, i);
-    new_head = s.buildTree(inorder, postorder);
-    // print(head);
-    Delete_Tree(head);
-    // print(new_head);
-    Delete_Tree(new_head);
+    Testcase("{3, 9, NULL, NULL, 15, NULL, NULL}\n",
+             inorder = {9, 3, 15}, postorder = {9, 15, 3});
+    Testcase("{3, 9, NULL, NULL, 20, 15, NULL, NULL, 7, NULL, NULL}\n",
+             inorder = {9, 3, 15, 20, 7}, postorder = {9, 15, 7, 20, 3});
+    Testcase("{-1, NULL, NULL}\n",
+             inorder = {-1}, postorder = {-1});
 }
