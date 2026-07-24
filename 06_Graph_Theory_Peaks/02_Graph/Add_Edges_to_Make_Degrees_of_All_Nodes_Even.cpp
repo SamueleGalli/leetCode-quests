dd6c7b6d7e4c8d13d2d7891da9a7b0c3643e9fb4
samuele-galli <samuele.aggeggio@gmail.com>
2026-07-24 11:44:33 +0200

    good graph working

--- 06_Graph_Theory_Peaks/02_Graph/Add_Edges_to_Make_Degrees_of_All_Nodes_Even.cpp
+++ 06_Graph_Theory_Peaks/02_Graph/Add_Edges_to_Make_Degrees_of_All_Nodes_Even.cpp
@@ -40,81 +40,50 @@ using namespace std;
 
 #include <iostream>
 #include <vector>
-#include <unordered_map>
 #include <unordered_set>
-#include <iterator>
-
-// TODO caso grande da un risultato errato solo 2  archi da sistemare controlalre anche 4
 
 class Solution
 {
 private:
-    bool find_next_valid(vector<int> &even_odd,
-                         unordered_map<int, unordered_set<int>> &graph, size_t &i, int odd = 1)
+    bool do_four(const vector<unordered_set<int>> &graph, const vector<int> &only_odd)
     {
-        unordered_map<int, unordered_set<int>>::iterator exist;
-        for (size_t j = 0; j < even_odd.size(); j++)
-        {
-            cout << "provo " << j << endl;
-            if (even_odd[j] % 2 == 1 && j != i)
-            {
-                cout << j << " e valido esiste gia?\n";
-                exist = graph.find(i);
-                cout << (exist != graph.end() ? "giusto" : "sbagliato") << endl;
-                if (exist != graph.end() && !exist->second.count(j))
-                {
-                    cout << (!exist->second.count(j) ? "giusto" : "sbagliato") << endl;
-                    graph[static_cast<int>(i)].insert(static_cast<int>(j));
-                    graph[j].insert(static_cast<int>(i));
-                    even_odd[i]++;
-                    even_odd[j]++;
-                    odd--;
-                    break;
-                }
-            }
-        }
-        if (odd > 0)
-            return (false);
-        return (true);
-    }
-    bool is_valid(vector<int> &even_odd,
-                  unordered_map<int, unordered_set<int>> &graph, int odd)
-    {
-        for (size_t i = 0; i < even_odd.size(); i++)
-        {
-            if (even_odd[i] % 2 == 1)
-            {
-                cout << "provo a cercare una coppia per " << i << endl;
-                if (find_next_valid(even_odd, graph, i) == false)
-                    return (false);
-            }
-        }
-        return (true);
+        if (!graph[only_odd[0]].count(only_odd[1]) && !graph[only_odd[2]].count(only_odd[3]))
+            return (true);
+        else if (!graph[only_odd[0]].count(only_odd[2]) && !graph[only_odd[1]].count(only_odd[3]))
+            return (true);
+        else if (!graph[only_odd[0]].count(only_odd[3]) && !graph[only_odd[1]].count(only_odd[2]))
+            return (true);
+        return (false);
     }
 
-    int is_invalid(vector<int> &even_odd, int odd = 0)
+    bool find_k(const vector<unordered_set<int>> &graph, const vector<int> &only_odd, int dim)
     {
-        for (size_t i = 0; i < even_odd.size(); i++)
+        for (int i = 1; i < dim; i++)
         {
-            if (even_odd[i] % 2 == 1)
-                odd++;
+            if (i != only_odd[0] && i != only_odd[1] &&
+                !graph[i].count(only_odd[0]) && !graph[i].count(only_odd[1]))
+                return (true);
         }
-        cout << "quanti sono odd?\n";
-        cout << odd << endl;
-        if (odd > 4 || odd == 0)
-            return (odd);
-        else
-            return (-1);
+        return (false);
     }
 
-public:
-    bool isPossible(int n, vector<vector<int>> &edges)
+    bool is_valid(const vector<unordered_set<int>> &graph, const vector<int> &only_odd, const int dim)
     {
-        unordered_map<int, unordered_set<int>> graph;
-        vector<int> even_odd(n + 1);
-        int odd = 0;
+        if (only_odd.size() == 4)
+            return (do_four(graph, only_odd));
+        else if (only_odd.size() == 2)
+        {
+            if (!graph[only_odd[0]].count(only_odd[1]))
+                return (true);
+            else
+                return (find_k(graph, only_odd, dim));
+        }
+        return (false);
+    }
 
-        graph.reserve(n);
+    void alloc_all(vector<unordered_set<int>> &graph, vector<int> &even_odd,
+                   vector<int> &only_odd, const vector<vector<int>> &edges)
+    {
         for (size_t i = 0; i < edges.size(); i++)
         {
             even_odd[edges[i][0]]++;
@@ -122,21 +91,31 @@ public:
             graph[edges[i][0]].insert(edges[i][1]);
             graph[edges[i][1]].insert(edges[i][0]);
         }
-        odd = is_invalid(even_odd);
-        switch (odd)
+
+        for (size_t i = 0; i < even_odd.size(); i++)
         {
-        case 0:
-            return (true);
-            break;
-
-        case -1:
-            return (is_valid(even_odd, graph, odd));
-            break;
-
-        default:
-            return (false);
-            break;
+            if (even_odd[i] % 2 == 1)
+                only_odd.push_back(static_cast<int>(i));
         }
+    }
+
+public:
+    bool isPossible(int n, vector<vector<int>> &edges)
+    {
+        vector<unordered_set<int>> graph(n + 1);
+        vector<int> even_odd(n + 1);
+        vector<int> only_odd;
+
+        only_odd.reserve(4);
+        for (unordered_set<int> &node : graph)
+            node.reserve(4);
+
+        alloc_all(graph, even_odd, only_odd, edges);
+
+        if (only_odd.size() == 0)
+            return (true);
+        else
+            return (is_valid(graph, only_odd, n));
     }
 };
 
@@ -153,10 +132,10 @@ int main()
 {
     vector<vector<int>> edges;
 
-    /*    testcase(5, edges = {{1, 2}, {2, 3}, {3, 4}, {4, 2}, {1, 4}, {2, 5}});
-       testcase(4, edges = {{1, 2}, {3, 4}});
-       testcase(4, edges = {{1, 2}, {1, 3}, {1, 4}});
-       testcase(4, edges = {{4, 1}, {3, 2}, {2, 4}, {1, 3}});
-       testcase(4, edges = {{4, 1}, {3, 2}}); */
-    testcase(21, edges = {{2, 19}, {16, 17}, {8, 14}, {2, 16}, {12, 20}, {12, 14}, {16, 18}, {15, 16}, {10, 21}, {3, 5}, {13, 18}, {17, 20}, {14, 17}, {9, 12}, {5, 15}, {5, 6}, {3, 7}, {2, 21}, {10, 13}, {8, 16}, {7, 18}, {4, 6}, {9, 1}, {13, 21}, {18, 20}, {7, 14}, {4, 19}, {5, 8}, {3, 11}, {11, 1}, {7, 12}, {4, 7}, {3, 16}, {13, 17}, {17, 19}, {9, 13}, {7, 19}, {10, 16}, {4, 13}, {4, 5}, {2, 15}, {12, 19}, {11, 16}, {2, 9}, {11, 17}, {17, 1}, {16, 21}, {4, 10}, {10, 14}, {14, 16}, {4, 1}, {13, 20}, {5, 20}, {4, 14}, {4, 21}, {10, 20}, {2, 14}, {8, 15}, {4, 8}, {6, 19}, {15, 1}, {19, 1}, {8, 19}, {15, 21}, {3, 12}, {11, 18}, {9, 17}, {18, 19}, {7, 21}, {3, 21}, {16, 19}, {11, 15}, {5, 1}, {8, 17}, {3, 15}, {8, 1}, {10, 19}, {3, 8}, {6, 16}, {2, 8}, {5, 18}, {11, 13}, {11, 20}, {14, 21}, {6, 20}, {4, 20}, {12, 13}, {5, 12}, {10, 11}, {9, 15}, {3, 19}, {9, 20}, {14, 18}, {21, 1}, {13, 19}, {8, 21}, {2, 13}, {3, 10}, {9, 18}, {19, 21}, {6, 7}, {3, 18}, {2, 18}, {6, 14}, {3, 17}, {5, 21}, {14, 20}, {8, 9}, {16, 1}, {3, 4}, {13, 1}, {5, 9}, {4, 15}, {17, 21}, {20, 21}, {2, 17}, {13, 14}, {11, 14}, {9, 16}, {10, 18}, {6, 15}, {6, 12}, {3, 13}, {5, 11}, {6, 1}, {12, 17}, {8, 10}, {5, 10}, {8, 18}, {4, 12}, {10, 1}, {6, 13}, {4, 18}, {7, 20}, {7, 16}, {2, 6}, {12, 21}, {4, 17}, {15, 18}, {13, 16}, {15, 20}, {7, 10}, {6, 10}, {2, 20}, {7, 15}, {18, 1}, {12, 1}, {3, 20}, {7, 1}, {14, 15}, {4, 9}, {11, 19}, {7, 9}, {5, 17}, {18, 21}, {6, 21}, {8, 11}, {6, 17}, {3, 14}, {7, 11}, {5, 7}, {7, 13}, {6, 8}, {6, 9}, {10, 12}, {5, 16}, {2, 4}, {17, 18}, {9, 11}, {12, 16}, {3, 6}, {12, 18}, {3, 9}, {11, 12}, {14, 19}, {10, 15}, {5, 13}, {8, 13}, {15, 17}, {2, 10}, {11, 21}, {20, 1}, {6, 18}, {2, 12}, {19, 20}, {6, 11}, {8, 12}, {2, 3}, {12, 15}, {2, 11}, {9, 10}, {7, 17}, {9, 19}, {13, 15}, {7, 8}, {4, 11}, {2, 5}, {5, 19}, {16, 20}, {15, 19}, {9, 14}, {14, 1}, {10, 17}, {9, 21}, {2, 7}, {8, 20}, {5, 14}, {4, 16}});
+    /*testcase(5, edges = {{1, 2}, {2, 3}, {3, 4}, {4, 2}, {1, 4}, {2, 5}});
+    testcase(4, edges = {{1, 2}, {3, 4}});
+    testcase(4, edges = {{1, 2}, {1, 3}, {1, 4}});
+    testcase(4, edges = {{4, 1}, {3, 2}, {2, 4}, {1, 3}});
+    testcase(4, edges = {{4, 1}, {3, 2}});*/
+    testcase(4, edges = {{1, 2}, {2, 3}, {2, 4}, {3, 4}});
 }
