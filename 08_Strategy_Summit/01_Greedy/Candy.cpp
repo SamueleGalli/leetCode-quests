9fa3208e4a971926d8153286e57ac33d4a2c239f
samuele-galli <samuele.aggeggio@gmail.com>
2026-09-11 12:51:12 +0200

    Finished Greedy algorithm

--- 08_Strategy_Summit/01_Greedy/Candy.cpp
+++ 08_Strategy_Summit/01_Greedy/Candy.cpp
@@ -32,9 +32,59 @@ using namespace std;
 
 class Solution
 {
+private:
+    int cout_dir(const vector<int> &ratings, size_t i, bool up, int count = 1)
+    {
+        while (i < ratings.size() && i + 1 < ratings.size())
+        {
+            if ((up && ratings[i + 1] <= ratings[i]) ||
+                (!up && ratings[i + 1] >= ratings[i]))
+                break;
+            count++;
+            i++;
+        }
+        if (count == 1)
+            return (-1);
+        return (count);
+    }
+
 public:
     int candy(vector<int> &ratings)
     {
+        int candy_counter = 0;
+        int descend = 0;
+        int ascend = 0;
+        vector<int> childs;
+
+        childs.resize(ratings.size(), 1);
+
+        for (size_t i = 0; i < ratings.size();)
+        {
+            ascend = cout_dir(ratings, i, true);
+            for (int j = 1; j < ascend && i + 1 < ratings.size(); j++)
+            {
+                childs[i + 1] = j + 1;
+                i++;
+            }
+
+            descend = cout_dir(ratings, i, false);
+
+            if (ascend > descend)
+            {
+                i++;
+                continue;
+            }
+
+            while (descend > 1)
+                childs[i++] = descend--;
+
+            if (descend == -1 && ascend == -1)
+                i++;
+        }
+        for (int child : childs)
+            candy_counter += child;
+
+        return (candy_counter);
     }
 };
 
@@ -49,6 +99,38 @@ int main()
     cout << "result = " << result << endl;
 
     ratings = {1, 2, 2};
+    result = s.candy(ratings);
+    cout << "result = " << result << endl;
+
+    ratings = {1, 3, 2, 2, 1};
+    result = s.candy(ratings);
+    cout << "result = " << result << endl;
+
+    ratings = {1, 2, 3, 2, 1};
+    result = s.candy(ratings);
+    cout << "result = " << result << endl;
+
+    ratings = {1, 6, 10, 8, 7, 3, 2};
+    result = s.candy(ratings);
+    cout << "result = " << result << endl;
+
+    ratings = {1, 2, 3};
+    result = s.candy(ratings);
+    cout << "result = " << result << endl;
+
+    ratings = {3, 2, 1};
+    result = s.candy(ratings);
+    cout << "result = " << result << endl;
+
+    ratings = {1, 2, 3, 4, 5, 4, 3, 2, 1};
+    result = s.candy(ratings);
+    cout << "result = " << result << endl;
+
+    ratings = {1, 2, 3, 2, 1, 2, 3};
+    result = s.candy(ratings);
+    cout << "result = " << result << endl;
+
+    ratings = {1, 3, 4, 5, 2};
     result = s.candy(ratings);
     cout << "result = " << result << endl;
 }
